acafbcbb32271086922b88f729d697341a1bd119
Jiajun Huang <gansteed@gmail.com>
2018-01-21 10:39:06 +0800

    seperate transport

--- proxy.go
+++ proxy.go
@@ -11,6 +11,22 @@ import (
 	"time"
 )
 
+var (
+	transport = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          2048,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		MaxIdleConnsPerHost:   2048,
+	}
+)
+
 // Proxy use httputil.ReverseProxy
 func Proxy(balancer Balancer, w ResponseWriter, r *http.Request) {
 	backend, found := balancer.Select()
@@ -24,19 +40,6 @@ func Proxy(balancer Balancer, w ResponseWriter, r *http.Request) {
 		req.URL.Host = backend.ToURL()
 		req.Header.Add("X-Real-IP", r.Host)
 		req.Header.Add("X-Forwarded-By", "Guard")
-	}
-	transport := &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 30 * time.Second,
-			DualStack: true,
-		}).DialContext,
-		MaxIdleConns:          2048,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-		MaxIdleConnsPerHost:   2048,
 	}
 	proxy := &httputil.ReverseProxy{
 		Director:  director,
